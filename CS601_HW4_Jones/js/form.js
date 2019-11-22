238d2f75493ac74c991050aa35748d8c21f898e3
petitlapin86 <skinnerpa86@yahoo.com>
2019-11-22 14:07:44 -0500

    work on form validation with javascript - following textbook exampkle

--- CS601_HW4_Jones/js/form.js
+++ CS601_HW4_Jones/js/form.js
@@ -1,33 +1,30 @@
-//form validation
-(function () {
+(function () { //form validation
   document.forms.register.noValidate = true; //disable html5 form validation
   $('form').on('submit', function(e){ //when form is submitted
     var elements =this.elements; //collect form controls
     var valid = {}; //custom valid object
     var isValid; //checks form controls
     var isFormValid; //checks entire form
-  }
-}
 
 //call functions outside of event handler
 for (var i = 0, 1 = (elements.length -1); i < 1; i++) {
   isValid = validateRequired(elements[i]) && validateTypes(elements[i]);
   if (!isValid) { //if it does not pass these 2 tests:
     showErrorMessage(elements[i]); //show error messages
-  }
-  else { //otherwise
+  } else { //otherwise
     removeErrorMessage(elements[i]); //remove error messages
   }
   valid[elements[i].id] =isValid; //add element to valid object
 } //end for loop
 
 //CUSTOM VALIDATION
-if (!validatePaigesForm()) {
+if (!validateBio()) {
   showErrorMessage(document.getElementById('bio'));
   valid.bio = false;
-} else }
+} else {
 removeErrorMessage(document.getElementById('bio'));
 }
+
 //DID IT PASS
 //CAN IT SUBMIT THE FORM?
 for (var field in valid) { //check properties of the valid object
@@ -40,4 +37,7 @@ for (var field in valid) { //check properties of the valid object
 //prevent submission
 if (!isFormValid) { //is is form valid is not true:
   e.preventDefault(); //prevent form from submitting
-}
+ }
+
+});
+}());
