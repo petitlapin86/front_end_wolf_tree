a6f1af70d66134e64650e8fc65257f9c2b6e2b74
petitlapin86 <skinnerpa86@yahoo.com>
2019-12-08 10:41:24 -0500

    comment js code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-window.onload=function(){
+window.onload=function(){ //onload fires when the document's window is ready for presentation and document.
 
 // JAVASCRIPT FUNCTIONALITY TO SHOW POPUP ON NYE BADGE CLICK (INDEX PAGE LINE 36)
 $(document).ready(function(){
@@ -9,11 +9,12 @@ $(document).ready(function(){
      $('.overlay-bg').show().css({'height' : docHeight});
      $('.popup'+whichpopup).show().css({'top': scrollTop+20+'px'});
     }
-    // function to close our popups
+
+    // function to close popup
     function closePopup(){
-        $('.overlay-bg, .overlay-content').hide();
+        $('.overlay-bg, .overlay-content').hide();   // hide popup
     }
-    $('.show-popup').click(function(event){
+    $('.show-popup').click(function(event){ //show popup
         event.preventDefault();
         var selectedPopup = $(this).data('showpopup');
         showPopup(selectedPopup);
@@ -22,13 +23,14 @@ $(document).ready(function(){
         closePopup();
     });
     $(document).keyup(function(e) {
-        if (e.keyCode == 27) {
+        if (e.keyCode == 27) { //jquery escape key is 27
             closePopup();
         }
     });
 });
 
 //FUNCTIONALITY TO SHOW JSON DATA ON BUTTON CLICK (MENU PAGE LINE 88)
+//UTILIZING WHAT WE LEARNED IN WEEK 5 about AJAX AND JSON
 const btn = document.getElementById('jsonOnClick'); //create a variable for the html button
 
 // MY FUNCTION TO DISPLAY JSON
@@ -45,7 +47,7 @@ function createTable(jsonObject) {
     return tableArray; //return the complete table
 }
 
-const getDegreeData = () => {
+const getPurveryorData = () => {
     const xhr = new XMLHttpRequest(); // Create XML request object
     // Use XMLHttpRequest method open to initialize newly created request or reinitialize and existing request
 
@@ -60,10 +62,8 @@ const getDegreeData = () => {
             btn.style.display = 'none'; //hide the purveyors button once data displays
         }
     };
-    // Open initializes the request, and send actually sends it
-    // Automatic Trashcan analogy: open is equivalent to trashcan opening and send is actually throwing something in the trash
-    xhr.send();
+    xhr.send(); // Open initializes the request, and send actually sends it
 };
 
-btn.addEventListener('click', getDegreeData); //listen for users click of the button
+btn.addEventListener('click', getPurveryorData); //listen for users click of the button
 }
